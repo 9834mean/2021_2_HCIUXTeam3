fe1b04653d06d91b82ad00106d625cbc7f07d9cf
201029PC\User <9834min@mediasol.kr>
2021-11-02 20:05:24 +0900

    작업하기 즐겁게 디자인 적용

--- JS/Main.js
+++ JS/Main.js
@@ -1,16 +1,27 @@
+IDList = [];
+
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://hciuxteam3-default-rtdb.firebaseio.com/Users.json');
 xhr.setRequestHeader('Content-type', 'application/json');
-xhr.send();
 
 xhr.onreadystatechange = function (e) {
     if (xhr.readyState === XMLHttpRequest.DONE) {
-      // status는 response 상태 코드를 반환 : 200 => 정상
-      if(xhr.status === 200) {
-        console.log(xhr.responseText);
-      } else {
-        console.log('Error!');
-      }
+        if (xhr.status === 200) {
+            TryData(JSON.parse(xhr.responseText));
+        } else {
+            console.log('Error!');
+        }
     }
-  };
-  
+};
+
+xhr.send();
+
+function TryData(data){
+    for(var i in data)
+    {
+        IDList.push(i)
+    }
+
+    console.log(IDList.length)
+}
+
