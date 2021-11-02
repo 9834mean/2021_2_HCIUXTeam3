75e0867b27553063ed2b56a7a00d1d70c317a9b6
201029PC\User <9834min@mediasol.kr>
2021-11-02 20:32:29 +0900

    로그인 시스템 구현 완료

--- JS/Main.js
+++ JS/Main.js
@@ -1,5 +1,5 @@
 IDList = [];
-
+var UserData;
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://hciuxteam3-default-rtdb.firebaseio.com/Users.json');
 xhr.setRequestHeader('Content-type', 'application/json');
@@ -16,12 +16,48 @@ xhr.onreadystatechange = function (e) {
 
 xhr.send();
 
-function TryData(data){
-    for(var i in data)
-    {
-        IDList.push(i)
-    }
 
+function TryData(data){
+    for(var i = 0; i<data.length; i++)
+    {
+        IDList.push(data[0]["ID"]);
+    }
+    UserData = data;
     console.log(IDList.length)
 }
 
+function LoginClick(){
+
+    var id = document.getElementById("txt_id").value;
+    var ps = document.getElementById("txt_ps").value;
+
+    if(id=="")
+    {
+        alert("아이디를 입력해 주세요!")
+    }
+    else if(ps=="")
+    {
+        alert("비밀번호를 입력해 주세요!")
+    }
+    else
+    {
+        n =IDList.indexOf(id) 
+        if(n>-1)
+        {
+            var shapw = CryptoJS.SHA256(ps).toString(); 
+            if(shapw==UserData[n]["PW"])
+            {
+                
+            }
+            else
+            {
+                alert("아이디/비밀번호를 확인해 주세요.")
+            }
+        }
+        else
+        {
+            alert("존재하지 않는 ID 입니다.")
+        }
+    }
+
+}
