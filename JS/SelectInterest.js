60620e1bf3de55ff230aad85b5f13fb3404ae254
201029PC\User <9834min@mediasol.kr>
2021-11-03 17:05:23 +0900

    값 가져오고, 선호분야 선택까지 완료 인제 RDB에 업데이트 하는 것만 남음

--- JS/SelectInterest.js
+++ JS/SelectInterest.js
@@ -1,4 +1,32 @@
+temp = location.href.split("?");
+id=temp[1].split("/");
+
+CheckData = [];
+
 function CompeleteClick()
 {
-    alert(Common_ID)
+    if(CheckData.length>1)
+    {
+        
+    }
+    else
+    {
+        alert("최소 하나의 분야를 선택해 주세요!")
+    }
+}
+
+function OnCheckClick(data)
+{
+    if(CheckData.includes(data.value))
+    {
+        var index = CheckData.indexOf(data.value)
+        if(index >-1)
+        {
+            CheckData.splice(index,1)
+        }
+    }
+    else
+    {
+        CheckData.push(data.value)
+    }
 }
--- JS/Signup.js
+++ JS/Signup.js
@@ -59,7 +59,7 @@ function SignUpClick()
         else
         {
             //선호 찾는 페이지로
-            location.href="SelectInterest.html";
+            location.href="SelectInterest.html?" + id + "/" + ps;
         }
     }
 
