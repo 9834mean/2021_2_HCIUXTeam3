63ef0da4503c178bf5c63c6c2642e8e402b1b2f6
201029PC\User <9834min@mediasol.kr>
2021-11-04 16:33:08 +0900

    회원가입 시스템 구축 완료

--- JS/SelectInterest.js
+++ JS/SelectInterest.js
@@ -7,15 +7,42 @@ function CompeleteClick()
 {
     if(CheckData.length>0)
     {
-        var data = {
+        var chdata = "";
+
+        for(var i = 0; i<CheckData.length; i++)
+        {
+            if(i==CheckData.length-1)
+            {
+                chdata+=CheckData[i];
+            }
+            else
+            {
+                chdata+=CheckData[i] + ",";
+            }
+        }
+
+        var senddata = {
             ID: param[0],
-            PW: param[1],
-            Interest: "",
+            PW:  CryptoJS.SHA256(param[1]).toString(),
+            Interest: chdata,
             ShowType:"A"
         };
-        
-        
 
+        $.ajax({
+            type: "put",
+            url : "https://hciuxteam3-default-rtdb.firebaseio.com/Users/" + param[2] +".json",
+            data: JSON.stringify(senddata),
+            dataType:'json', 
+            success: function(result){
+                //작업이 성공적으로 발생했을 경우
+                alert("회원가입 성공")
+                location.href="Main.html";
+            },
+            error:function(){  
+                //에러가 났을 경우 실행시킬 코드
+                alert("회원가입 실패")
+            }
+        })
     }
     else
     {
