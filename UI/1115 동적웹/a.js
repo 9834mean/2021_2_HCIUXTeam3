c77b26e961698090369e11d2cc9093daf408bea5
201029PC\User <9834min@mediasol.kr>
2021-11-15 20:16:34 +0900

    태그 추가

--- UI/1115 동적웹/a.js
+++ UI/1115 동적웹/a.js
@@ -1,20 +1,10 @@
-const img= document.querySelector(".imgg");
-document.body.onload = addElement;
-
-function create_image(){
-  let image = document.createElement('image');
- 
-  image.setAttribute('class', 'pTag');
-  var imagee = document.createTextNode("생길 이미지");
-  image.appendChild(imagee);
- 
-
+for (i = 0; i < 100; i++) {
+  document.write('<article class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xxs-12 animate-box">')
+  document.write('<figure>')
+  document.write('<a href="https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=100&oid=001&aid=0012792321"><img width="380" Height="380" src="https://imgnews.pstatic.net/image/origin/001/2021/11/15/12792321.jpg?type=ofullfill106_72" alt="Image" class="img-responsive"></a>')
+  document.write('</figure>')
+  document.write('<span class="fh5co-meta"><a href="single.html">' + i + '&amp; Drink</a></span>')
+  document.write('<h2 class="fh5co-article-title"><a href="https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=100&oid=001&aid=0012792321">We Eat and Drink All Night</a></h2>')
+  document.write('<span class="fh5co-meta fh5co-date">March 6th, 2016</span>')
+  document.write('</article>')
 }
-function create_title(){
-    let title = document.createElement('title');
-   
-    title.setAttribute('class', 'pTag');
-    var titlee = document.createTextNode("생길 이미지");
-    image.appendChild(titlee);
-}
-
