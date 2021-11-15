9b1164cbb84242fcdd9284d8c79e8bc9c5534c7a
201029PC\User <9834min@mediasol.kr>
2021-11-15 20:22:59 +0900

    Revert "Revert "릴리즈 Start""
    This reverts commit f492d8fb95d0fc696bf9d235f7ac233162020fc8.

--- Release/Main/a.js
+++ Release/Main/a.js
@@ -4,7 +4,7 @@ for (i = 0; i < 100; i++) {
   document.write('<a href="https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=100&oid=001&aid=0012792321"><img width="380" Height="380" src="https://imgnews.pstatic.net/image/origin/001/2021/11/15/12792321.jpg?type=ofullfill106_72" alt="Image" class="img-responsive"></a>')
   document.write('</figure>')
   document.write('<span class="fh5co-meta"><a href="single.html">' + i + '&amp; Drink</a></span>')
-  document.write('<h2 class="fh5co-article-title"><a href="single.html">We Eat and Drink All Night</a></h2>')
+  document.write('<h2 class="fh5co-article-title"><a href="https://news.naver.com/main/read.naver?mode=LSD&mid=sec&sid1=100&oid=001&aid=0012792321">We Eat and Drink All Night</a></h2>')
   document.write('<span class="fh5co-meta fh5co-date">March 6th, 2016</span>')
   document.write('</article>')
 }
